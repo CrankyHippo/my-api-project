70f22907f424c064cfa0b7fbc32661ce719b88b3
Bryan Burnett <crankyhippo@gmail.com>
2021-07-01 12:32:32 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Posts from "./Posts.js";
+import Countries from "./countries";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        
+        <h1>React API Practice Project</h1>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://jsonplaceholder.typicode.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          API Post Source: JSON Placeholder
+        </a>
+        <a
+          className="App-link"
+          href="https://restcountries.eu/#api-endpoints-all"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          API Countries Source: REST COUNTRIES
         </a>
       </header>
+      <Posts />
+      <hr></hr>
+      <Countries />
     </div>
   );
 }
